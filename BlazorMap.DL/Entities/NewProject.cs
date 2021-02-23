3ea5da062337b99a23baecfc1effdcc89b48ef28
Allkman <algirdas.cern@gmail.com>
2021-02-23 15:29:17 +0200

    Add project files.

---
+++ BlazorMap.DL/Entities/NewProject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMap.DL.Entities
+{
+    public class NewProject
+    {
+        public string Id { get; set; }
+        public string ProjectName { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
